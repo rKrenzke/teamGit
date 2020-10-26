cedb4e8611dd05f7e3f65824e239c92480995196
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-10-26 15:37:15 -0400

    react boilerplate

---
+++ alldayproject/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  //state variable set to location
+  return (
+    <div className="App">
+      <Nasa />
+      <Weather />
+      <Zomato />
+    </div>
+  );
+}
+
+export default App;
