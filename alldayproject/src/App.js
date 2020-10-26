a9ee2200492d3e96421deb7721268ee4c9fd0a42
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-10-26 16:31:55 -0400

    lat and long

--- alldayproject/src/App.js
+++ alldayproject/src/App.js
@@ -2,26 +2,29 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  //state variable set to location
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
   const getLocation = () => {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-     function success(position) {
-      setLatitude(position.coords.latitude); 
-      setLongitude(position.coords.longitude);
-     },
-    function error(error_message) {
-      console.error('An error has occured while retrieving location', error_message)
-    });
-  } else {
-    console.log('geolocation is not enabled on this browser')
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function success(position) {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        function error(error_message) {
+          console.error('An error has occured while retrieving location', error_message)
+        });
+    } else {
+      console.log('geolocation is not enabled on this browser')
+    };
   };
-};
+
   return (
     <div className="App">
+      <Nasa longitude={longitude} latitude={latitude} />
+      <Weather longitude={longitude} latitude={latitude} />
+      <Zomato longitude={longitude} latitude={latitude} />
     </div>
   );
 }
