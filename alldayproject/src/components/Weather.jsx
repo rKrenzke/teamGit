1bd49683829358e2a2b2fbd0a046aacc17b10f92
shafferma <shafferma12@gmail.com>
2020-10-26 17:38:04 -0400

    weather start

---
+++ alldayproject/src/components/Weather.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from 'reactstrap'; 
+
+// ?id={city id}&appid={API key}
+
+const Weather = (props) => {
+ 
+}
+
+const baseUrl = 'api.openweathermap.org/data/2.5/weather'
+
+return (
+    <div>
+        <Button type="submit"></Button>
+    </div>
+)
+
+
+export default Weather;
